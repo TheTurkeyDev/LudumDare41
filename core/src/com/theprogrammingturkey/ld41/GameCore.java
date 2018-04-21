37490d5f238ae0f8d88de4fef05b832d9d4a7520
Turkey <turkey2349@gmail.com>
2018-04-20 23:13:08 -0400

    Added hookin to allow for tasks to work

--- core/src/com/theprogrammingturkey/ld41/GameCore.java
+++ core/src/com/theprogrammingturkey/ld41/GameCore.java
@@ -2,16 +2,18 @@ package com.theprogrammingturkey.ld41;
 
 import com.badlogic.gdx.Game;
 import com.theprogrammingturkey.ld41.rendering.GameScreen;
+import com.theprogrammingturkey.volatiliajava.task.Scheduler;
 
 public class GameCore extends Game {
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		super.setScreen(new GameScreen());
 	}
 
 	@Override
-	public void render () {
+	public void render() {
+		Scheduler.tickSyncTasks();
 		super.render();
 	}
 
