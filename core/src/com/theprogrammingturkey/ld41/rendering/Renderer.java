262cc8c9818c1a9674ea3258c7f1414280882d98
Turkey <turkey2349@gmail.com>
2018-04-21 00:38:54 -0400

    Adding buttons and main screen

--- core/src/com/theprogrammingturkey/ld41/rendering/Renderer.java
+++ core/src/com/theprogrammingturkey/ld41/rendering/Renderer.java
@@ -1,10 +1,16 @@
 package com.theprogrammingturkey.ld41.rendering;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
 public class Renderer {
 
@@ -18,12 +24,13 @@ public class Renderer {
 	public static void endBatch() {
 		batch.end();
 	}
-	
+
 	public static void drawSprite(Sprite sprite) {
 		sprite.draw(batch);
 	}
-	
-	public static TextureRegion createTextureRegion(int x, int y, int w, int h) {
+
+	public static TextureRegion createTextureRegion(int x, int y, int w,
+			int h) {
 		return new TextureRegion(spriteSheet, x, y, w, h);
 	}
 
@@ -32,9 +39,24 @@ public class Renderer {
 	}
 
 	public static Animation<TextureRegion> createAnimation() {
-		Animation<TextureRegion> animation = new Animation<TextureRegion>(1,createTextureRegion(1,1,100,100));
-		
+		Animation<TextureRegion> animation = new Animation<TextureRegion>(1,
+				createTextureRegion(1, 1, 100, 100));
+
 		return animation;
 	}
-	
+
+	public static TextButton getCustomButton(String text) {
+		BitmapFont font = new BitmapFont();
+		Skin skin = new Skin();
+		TextureAtlas buttonAtlas = new TextureAtlas(
+				Gdx.files.internal("ui_components/ButtonPack.txt"));
+		skin.addRegions(buttonAtlas);
+		TextButtonStyle textButtonStyle = new TextButtonStyle();
+		textButtonStyle.font = font;
+		textButtonStyle.up = skin.getDrawable("ButtonNormal");
+		textButtonStyle.down = skin.getDrawable("ButtonSelected");
+		textButtonStyle.checked = skin.getDrawable("ButtonChecked");
+		return new TextButton(text, textButtonStyle);
+	}
+
 }
---
+++ core/src/com/theprogrammingturkey/ld41/screens/MainScreen.java
@@ -0,0 +1,43 @@
+package com.theprogrammingturkey.ld41.screens;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+
+public class MainScreen implements Screen {
+	private TextButton start;
+	@Override
+	public void show() {
+		
+	}
+
+	@Override
+	public void render(float delta) {
+		
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		
+	}
+
+	@Override
+	public void pause() {
+		
+	}
+
+	@Override
+	public void resume() {
+		
+	}
+
+	@Override
+	public void hide() {
+		
+	}
+
+	@Override
+	public void dispose() {
+		
+	}
+
+}
