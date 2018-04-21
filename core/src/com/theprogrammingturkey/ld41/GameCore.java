f5aa93552e4fad39941da0c7e12c307fdb8073f4
Ian Willis <ianwillis98@gmail.com>
2018-04-21 00:59:05 -0400

    Clear's before render now

--- core/src/com/theprogrammingturkey/ld41/GameCore.java
+++ core/src/com/theprogrammingturkey/ld41/GameCore.java
@@ -1,24 +1,27 @@
 package com.theprogrammingturkey.ld41;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.theprogrammingturkey.ld41.screens.MainScreen;
 import com.theprogrammingturkey.volatiliajava.task.Scheduler;
 
 public class GameCore extends Game {
 
-	@Override
-	public void create() {
-		super.setScreen(new MainScreen(this));
-	}
+    @Override
+    public void create() {
+        super.setScreen(new MainScreen(this));
+    }
 
-	@Override
-	public void render() {
-		Scheduler.tickSyncTasks();
-		super.render();
-	}
+    @Override
+    public void render() {
+        Scheduler.tickSyncTasks();
+        Gdx.gl.glClearColor(1, 0, 1, 1);
+        Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+        super.render();
+    }
 
-	@Override
-	public void dispose() {
+    @Override
+    public void dispose() {
 
-	}
+    }
 }
